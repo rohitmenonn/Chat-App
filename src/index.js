0cee3ff775a84ace4f0f4b99c9285e1956a16d94
Rohit Menon <58653638+rohitmenonn@users.noreply.github.com>
2021-08-03 17:14:17 +0530

    Setup socket rooms

--- src/index.js
+++ src/index.js
@@ -17,8 +17,11 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket) => {
     console.log('New connection established')
 
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+    socket.on('join', ({ username, room }) => {
+        socket.join(room)
+        socket.emit('message', generateMessage('Welcome!'))
+        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+    })
 
     socket.on('sendMessage', (message, callback) => {
         io.emit('message', generateMessage(message))
