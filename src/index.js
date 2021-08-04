c99de34b2ae2d572b36a14bc85776763f62a9f75
Rohit Menon <58653638+rohitmenonn@users.noreply.github.com>
2021-08-04 23:44:25 +0530

    Add user tracking for joining and leaving rooms

--- src/index.js
+++ src/index.js
@@ -11,16 +11,25 @@ const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public') 
 
 const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, getUser, removeUser, getUsersInRoom } = require('./utils/users')
 
 app.use(express.static(publicDirectoryPath));
 
 io.on('connection', (socket) => {
     console.log('New connection established')
 
-    socket.on('join', ({ username, room }) => {
-        socket.join(room)
+    socket.on('join', ({ username, room }, callback) => {
+        const {user, error} = addUser({ id: socket.id, username, room })
+
+        if(error) {
+            return callback(error)
+        }
+
+        socket.join(user.room)
+
         socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+        callback()
     })
 
     socket.on('sendMessage', (message, callback) => {
@@ -34,7 +43,10 @@ io.on('connection', (socket) => {
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has left!'))
+        const user = removeUser(socket.id)
+        if(user){
+            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+        }
     })
 })
 
